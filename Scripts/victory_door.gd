2d8d779878a86a85c574852d371cb27e5f877949
Parsa <142070561+ParsaHP2@users.noreply.github.com>
2026-09-06 02:54:04 -0700

    more SFX

--- Scripts/victory_door.gd
+++ Scripts/victory_door.gd
@@ -2,7 +2,13 @@ extends Area2D
 
 signal level_completed
 
+var _completed := false
+
 
 func _on_body_entered(body: Node2D) -> void:
+	if _completed:
+		return
 	if body.is_in_group("Player"):
+		_completed = true
+		$VictorySFX.play()
 		level_completed.emit()
